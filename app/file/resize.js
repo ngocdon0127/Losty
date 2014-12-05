b8a0f742a1f2f98b139813fad10e93abfa41620e
cuongvu <cuongvc93@gmail.com>
2014-12-05 12:12:32 +0700

    check fullpath

--- app/file/resize.js
+++ app/file/resize.js
@@ -3,18 +3,17 @@ var url = require('url');
 var domain = require('./../../config/default').domain_default;
 var im = require('imagemagick');
 var gm = require('gm');
-var path = require('path');
 
 
 
 module.exports 					=	function(url_, callback){
 	console.log('./public/small_size' + url.parse(url_).path);
-	console.log('Full URL : ', process.env.PATH.split(path.delimiter));
 	gm('./public' + url.parse(url_).path)
 		.resize(120, 120)
 		.autoOrient()
 		.write('./public/small_size' + url.parse(url_).path, function (err) {
 			if (!err) console.log(' hooray! ');
+			console.log(err);
 		})
 	console.log(domain + '/small_size' + url.parse(url_).path);
 	callback(domain + '/small_size' + url.parse(url_).path);
