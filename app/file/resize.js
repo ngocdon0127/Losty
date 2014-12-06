3a2599ee981d9e2172a11b72a8aa26c8b9e6be8a
cuongvu <cuongvc93@gmail.com>
2014-12-06 11:20:14 +0700

    add normal size

--- app/file/resize.js
+++ app/file/resize.js
@@ -5,10 +5,7 @@ var im = require('imagemagick');
 var gm = require('gm').subClass({ imageMagick: true });
 // var gm = require('gm');
 
-
-
 module.exports 					=	function(url_, callback){
-	console.log('./public/small_size' + url.parse(url_).path);
 	gm('./public' + url.parse(url_).path)
 		.resize(120, 120)
 		.autoOrient()
--- app/file/resize_normal.js
+++ app/file/resize_normal.js
@@ -5,19 +5,17 @@ var im = require('imagemagick');
 var gm = require('gm').subClass({ imageMagick: true });
 // var gm = require('gm');
 
-
-
 module.exports 					=	function(url_, callback){
 	gm('./public' + url.parse(url_).path)
 		.resize(640, 640)
 		.autoOrient()
-		.write('./public/normal' + url.parse(url_).path, function (err) {
+		.write('./public/normal_size' + url.parse(url_).path, function (err) {
 			if (err) {
 				console.log(err);
 			}
 			else{
 				console.log('RESIZE SUCCESS');
-				callback(domain + '/normal' + url.parse(url_).path);
+				callback(domain + '/normal_size' + url.parse(url_).path);
 			}
 		})
 }
