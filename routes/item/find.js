fd71555ac6d08d470d58d8b032e667f963b90720
cuongvu <cuongvc93@gmail.com>
2014-12-16 10:46:32 +0700

    add find_item

--- routes/item/find.js
+++ routes/item/find.js
@@ -48,7 +48,7 @@ module.exports           =    function(req, res){
 							if (item.title.toLowerCase().indexOf(title.toLowerCase()) != -1){
 								console.log('distance : ', distance(item.location, location));
 								if (distance(item.location, location) <= distance_max){
-									if (check_day(item.date_lost, date_lost)){
+									if (check_day(new Date(item.date_lost), new Date(date_lost) ) ){
 										items.push(item);
 									}
 								}
