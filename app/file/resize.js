bd0a97ed13b37db04b6c146b085caf49d24031b3
cuongvu <cuongvc93@gmail.com>
2014-12-25 08:43:20 +0700

    fix multi upload photo

--- app/file/resize.js
+++ app/file/resize.js
@@ -3,11 +3,14 @@ var url = require('url');
 var domain = require('./../../config/default').domain_default;
 var im = require('imagemagick');
 
-// var gm = require('gm').subClass({ imageMagick: true });	    // gm with server
+var gm = require('gm').subClass({ imageMagick: true });	    // gm with server
 
-var gm = require('gm');																			// gm with location
+// var gm = require('gm');																			// gm with location
 	
 module.exports 					=	function(url_, callback){
+	console.log('url : "./public' + url.parse(url_).path, '"');
+	console.log('./public/small_size' + url.parse(url_).path);
+
 	gm('./public' + url.parse(url_).path)
 		.resize(120, 120)
 		.autoOrient()
