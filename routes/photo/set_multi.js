67d0e01b5007de1aa939a3b3eb57a5327d77aa1f
cuongvu <cuongvc93@gmail.com>
2014-12-28 10:54:06 +0700

    fix upload multi photo

--- routes/photo/set_multi.js
+++ routes/photo/set_multi.js
@@ -66,18 +66,17 @@ module.exports 			=	function(req, res){
                     ], function(err){
                       photo.save(function(err){
 
-                        i = i + 1;
-                        if (i == Object.keys(files).length - 1){
-                          res.json({error_code : 0});
-                          res.status(200).end();
-                          return 1;
-                        }
-
                         if (err){
                           res.json({error_code : 402, msg : err.toString()});     //  Database cannot 
                           res.status(200).end()       //  save
                         } else{
                           console.log('save photo successsss');
+                          i = i + 1;
+                          if (i == Object.keys(files).length - 1){
+                            res.json({error_code : 0});
+                            res.status(200).end();
+                            return 1;
+                          }
                         }
                       });
                     });
