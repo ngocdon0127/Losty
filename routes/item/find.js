a53f3fc319a5e0caf3bb3282bec30e5bc635709b
cuongvu <cuongvc93@gmail.com>
2014-12-29 17:05:46 +0700

    fix find item

--- routes/item/find.js
+++ routes/item/find.js
@@ -16,6 +16,9 @@ function check_day(time1, time2, timezone){
 	time1.setHours(time1.getHours() + timezone + 5);
 	time2.setHours(time2.getHours() + timezone + 5);
 
+	console.log(time1);
+	console.log(time2);
+
 	console.log(time1.getMonth(), time2.getMonth());
 	console.log(time1.getDate(),  time2.getDate());
 	console.log(time1.getFullYear(),  time2.getFullYear());
@@ -37,7 +40,7 @@ module.exports           =    function(req, res){
 		var data 		 		= req.body;
 		var location 		= data.location;
 		var category_id = data.category_id;
-		var date_lost 	= new Date(data.date);
+		var date_lost 	= data.date;
 		var title     	= data.title;
 		var type 				= data.type;
 		var timezone    = parseInt(data.timezone);
