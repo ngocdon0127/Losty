bdb3defe5c82925f0becb2a66a3d700d0b109c12
cuongvu <cuongvc93@gmail.com>
2015-01-05 10:47:52 +0700

    fix error

--- app/file/resize_small.js
+++ app/file/resize_small.js
@@ -7,6 +7,7 @@ var gm = require('gm').subClass({ imageMagick: true });	    // gm with server
 // var gm = require('gm');																			// gm with location
 	
 module.exports 					=	function(url_, callback){
+	console.log(url.parse(url_).path);
 	gm('./public' + url.parse(url_).path)
 		.resize(120, 120)
 		.autoOrient()
