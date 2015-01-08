f581855baa6cfa6e9fca2edb078e4e02331f7c30
cuongvu <cuongvc93@gmail.com>
2015-01-08 08:29:58 +0700

    fix upload photo

--- app/file/resize_normal.js
+++ app/file/resize_normal.js
@@ -2,11 +2,11 @@ var fs = require('fs');
 var path = require('path');
 var domain = require('./../../config/default').domain_default;
 var im = require('imagemagick');
-var gm = require('gm').subClass({ imageMagick: true });
 var url = require('url');
 // var gm = require('gm');
 
 module.exports 					=	function(url_, type, callback){
+	var gm = require('gm').subClass({ imageMagick: true });
 	gm('./public' + url.parse(url_).path)
 		.resize(640, 640)
 		.autoOrient()
