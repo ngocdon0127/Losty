21f095e27276c587b5caea08e6419e22112cdbe0
cuongvu <cuongvc93@gmail.com>
2015-01-09 08:25:31 +0700

    fix upload photo

--- app/file/resize_normal.js
+++ app/file/resize_normal.js
@@ -6,16 +6,36 @@ var url = require('url');
 // var gm = require('gm');
 
 module.exports 					=	function(url_, type, callback){
-	var gm = require('gm').subClass({ imageMagick: true });
-	gm('./public' + url.parse(url_).path)
-		.resize(640, 640)
-		.autoOrient()
-		.write('./public/img/normal_size/' + type +  '/' + path.basename(url_), function (err) {
-			if (err) {
-				console.log('Error : ', err);
-			}
-			else{
-				callback(domain + '/img/normal_size/' + type + '/' + path.basename(url_));
-			}
-		})
+	// var gm = require('gm').subClass({ imageMagick: true });
+	// gm('./public' + url.parse(url_).path)
+	// 	.resize(640, 640)
+	// 	.autoOrient()
+	// 	.write('./public/img/normal_size/' + type +  '/' + path.basename(url_), function (err) {
+	// 		if (err) {
+	// 			console.log('Error : ', err);
+	// 		}
+	// 		else{
+	// 			callback(domain + '/img/normal_size/' + type + '/' + path.basename(url_));
+	// 		}
+	// 	})
+
+	require('lwip').open('./public' + url.parse(url_).path, function(err, image){
+
+	  // check err...
+	  // define a batch of manipulations and save to disk as JPEG:
+	  image.batch()
+	    .scale(0.75)          // scale to 75%
+	    .rotate(45, 'white')  // rotate 45degs clockwise (white fill)
+	    .crop(640, 640)       // crop a 200X200 square from center
+	    .blur(5)              // Gaussian blur with SD=5
+	    .writeFile('./public/img/normal_size/' + type + '/' + path.basename(url_) , function(err){
+	    	if (err){
+	    		console.log('Error : ', err);
+	    	}
+	      // check err...
+	      // done.
+	    });
+
+	});
+
 }
--- app/file/resize_small.js
+++ app/file/resize_small.js
@@ -7,16 +7,32 @@ var path = require('path');
 	
 module.exports 					=	function(url_, type,  callback){
 	console.log('Writing to locate : "./public/img/small_size/' + type + '/' + path.basename(url_) , '"');
-	var gm = require('gm').subClass({ imageMagick: true });	    // gm with server
-	gm('./public' + url.parse(url_).path)
-		.resize(120, 120)
-		.autoOrient()
-		.write('./public/img/small_size/' + type + '/' + path.basename(url_), function (err) {
-			if (err) {
-				console.log('Error : ', err);
-			}
-			else{
-				callback(domain + '/img/small_size/' + type + '/' + path.basename(url_));
-			}
-		})
+	// var gm = require('gm').subClass({ imageMagick: true });	    // gm with server
+	// gm('./public' + url.parse(url_).path)
+	// 	.resize(120, 120)
+	// 	.autoOrient()
+	// 	.write('./public/img/small_size/' + type + '/' + path.basename(url_), function (err) {
+	// 		if (err) {
+	// 			console.log('Error : ', err);
+	// 		}
+	// 		else{
+	// 			callback(domain + '/img/small_size/' + type + '/' + path.basename(url_));
+	// 		}
+	// 	})
+
+	require('lwip').open('./public' + url.parse(url_).path, function(err, image){
+	  image.batch()
+	    .scale(0.75)          // scale to 75%
+	    .rotate(45, 'white')  // rotate 45degs clockwise (white fill)
+	    .crop(200, 200)       // crop a 200X200 square from center
+	    .blur(5)              // Gaussian blur with SD=5
+	    .writeFile('./public/img/small_size/' + type + '/' + path.basename(url_) , function(err){
+	    	if (err){
+	    		console.log('Error : ', err);
+	    	}
+	      // check err...
+	      // done.
+	    });
+	});
+
 }
